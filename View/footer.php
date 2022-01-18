4052f3109ab3f52e3de5735a56984ca5fc44828b
Jsalgado2411 <92396420+Jsalgado2411@users.noreply.github.com>
2022-01-17 18:54:27 -0600

    menu principal

--- View/footer.php
+++ View/footer.php
@@ -1,8 +1,13 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg footer">
+<br>
+       <br>
+       <br>
+       <br>
+       <br>
+      
 
-<p class="navbar-brand">CONAVIM &copy; 2021</p>
 </nav>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+       
 
     </body>
 </html>
--- View/header.php
+++ View/header.php
@@ -7,25 +7,47 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-    </head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/baner.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      </head>
     <body>
-        <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-primary bg-light">
+    
+    <div class="container">
+    <nav class="navbar navbar-expand-lg  barra">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="?c=home&a=Index">
+    <img src="assets/images/conavimV2.png" alt="" width="120" height="120">
+    </a>
+    &nbsp &nbsp &nbsp &nbsp &nbsp
+    &nbsp &nbsp &nbsp &nbsp &nbsp
+    &nbsp &nbsp &nbsp &nbsp &nbsp
+    &nbsp &nbsp &nbsp &nbsp &nbsp
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon">
+      <img src="assets/images/menu.png" alt="" width="30" height="30">
+      </span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
+      <div class="navbar-nav" >
+        <a class="nav-link active" aria-current="page" href="?c=home&a=Index"><img src="assets/images/home.png" class="image" alt="" width="70" height="70" data-bs-toggle="tooltip" data-bs-placement="bottom" title="HOME"></a>&nbsp &nbsp &nbsp &nbsp &nbsp
+        <a class="nav-link" href="#"><img src="assets/images/violencia-fisica.png" class="image" alt="ee"  width="70" height="70" data-bs-toggle="tooltip" data-bs-placement="bottom" title="VIOLENCIA DE GÉNERO"></a>&nbsp &nbsp &nbsp &nbsp
+        <a class="nav-link" href="#"><img src="assets/images/boton-de-emergencia.png" class="image" alt=""  width="70" height="70" data-bs-toggle="tooltip" data-bs-placement="bottom" title="BOTONES DE EMERGENCIA" ></a>&nbsp &nbsp &nbsp &nbsp
+        <a class="nav-link" href="#"><img src="assets/images/proteccion.png" class="image" alt=""  width="70" height="70" data-bs-toggle="tooltip" data-bs-placement="bottom" title="MEDIDAS DE PROTECCIÓN"></a>&nbsp &nbsp &nbsp &nbsp
+        <a class="nav-link" href="#"><img src="assets/images/alerta.png"  class="image"alt=""  width="70" height="70" data-bs-toggle="tooltip" data-bs-placement="bottom" title="ALERTAMIENTOS DE RIESGO"></a>&nbsp &nbsp &nbsp &nbsp
+        <a class="nav-link" href="#"><img src="assets/images/agresor.png" class="image" alt=""  width="70" height="70" data-bs-toggle="tooltip" data-bs-placement="bottom" title="IDENTIFICACIÓN DE AGRESORES"></a>&nbsp &nbsp &nbsp &nbsp
+        <a class="nav-link" href="#"><img src="assets/images/gps.png" class="image"  alt=""  width="70" height="70" data-bs-toggle="tooltip" data-bs-placement="bottom" title="GEOREFERENCIAS"></a>   &nbsp &nbsp &nbsp &nbsp
+
       </div>
     </div>
   </div>
 </nav>
 </div>
+<script>
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+</script>
+
     <div class="container">
